539f1339cf96ef0a4c1426ea0b7328ffba31f304
abonek <denis.horvat@student.mev.hr>
2026-04-09 16:12:14 +0200

    Add files via upload

---
+++ Korisnik.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MojaPrvaAplikacija.Models
+{
+    public class Korisnik
+    {
+        [Required]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string email { get; set; }
+        public DateTime datumStvaranjaRacuna { get; set; }
+        public bool admin { get; set; }
+
+    }
+}
